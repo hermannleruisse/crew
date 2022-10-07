d051ce2d797ab758fcb1b1e9c5494b9639d98e9a
Hermann <adjeakakpo@gmail.com>
2022-10-07 17:44:42 +0000

    modal ajout edition ministere

--- src/app/components/ministere/ministere.component.ts
+++ src/app/components/ministere/ministere.component.ts
@@ -1,10 +1,13 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { Ministere } from 'src/app/models/ministere';
 import { ToolService } from 'src/app/services/tool.service';
-// import * as M from 'materialize-css';
 import Swal from 'sweetalert2';
-import { of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogMinisterAddComponent } from '../dialog-minister-add/dialog-minister-add.component';
+import { ApiService } from 'src/app/services/api.service';
+import { DialogMinisterEditComponent } from '../dialog-minister-edit/dialog-minister-edit.component';
+import { Url } from 'src/app/models/url';
 
 @Component({
   selector: 'app-ministere',
@@ -12,153 +15,87 @@ import { of } from 'rxjs';
   styleUrls: ['./ministere.component.scss']
 })
 export class MinistereComponent implements OnInit {
-  ministerForm: FormGroup;
-  editModeMinisterForm: FormGroup;
-  editFormTitle?:string;
-  @ViewChild('closeBtnEdit') closeBtnEdit: ElementRef;
-
+  
   minister: Ministere = {
     id: '',
     code: '',
     libelle: '',
     description: ''
   };
-  editMode: boolean = false;
-  get code(){ return this.ministerForm.get("code");}
-  get libelle(){ return this.ministerForm.get("libelle");}
-
-  get idEdit(){ return this.editModeMinisterForm.get("id");}
-  get codeEdit(){ return this.editModeMinisterForm.get("code");}
-  get libelleEdit(){ return this.editModeMinisterForm.get("libelle");}
-
+ 
   ministeres = [];
+  displayedColumns: string[] = ['code', 'libelle', 'description', 'option'];
 
-  constructor(private formBuilder: FormBuilder, private toolService:ToolService) {
+  constructor(private toolService:ToolService, private apiService:ApiService, public dialog: MatDialog) {
   }
 
-  /**
-   * Initialisation du formulaire d'ajout de profile avec les validations
-   */
-   createMinisterForm(){
-    this.ministerForm = this.formBuilder.group({
-      code: [null, [Validators.required]],
-      libelle: [null, [Validators.required]],
-      description: [null]
-    },{
-      updateOn: 'blur'
+  openDialogAddMinister() {
+    const dialogRef = this.dialog.open(DialogMinisterAddComponent, {
+      height: '400px',
+      width: '600px',
+      disableClose: true });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+      this.getMinistersList();
     });
   }
 
-  /**
-   * Initialisation du formulaire d'edition de profile avec les validations
-   */
-  editMinisterForm(){
-    this.editModeMinisterForm = this.formBuilder.group({
-      id: [{ value: '', disabled:true}],
-      code: [{ value: '', disabled:true}, [Validators.required]],
-      libelle: [{ value: '', disabled:true}, [Validators.required]],
-      description: [{ value: '', disabled:true}]
-    },{
-      updateOn: 'change'
+  openDialogEditMinister(minister: Ministere) {
+    
+    const dialogRef = this.dialog.open(DialogMinisterEditComponent, { 
+      height: '400px',
+      width: '600px',
+      data : {
+        selectedMinister: minister
+      }, disableClose: true 
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+      this.getMinistersList();
     });
   }
 
   ngOnInit(): void {
-    // M.AutoInit();
-    console.log("profile");
-    this.createMinisterForm();
-    this.editMinisterForm();
+    this.getMinistersList();
+  }
 
-    //async ministere
-    of(this.getMinisteres()).subscribe(ministeres => {
-      this.ministeres = ministeres;
-    });
+  getMinistersList(){
+    this.toolService.showLoading();
+    this.apiService.get(Url.PROFILE_LIST_URL, {}).subscribe(
+      (data) => {
+        console.log('data => ' + JSON.stringify(data));
+        this.ministeres = data;
+      }, (error) => {
+        console.log('erreur ' + JSON.stringify(error));
+        this.toolService.hideLoading();
+        this.toolService.showToast(error.message, 'OK');
+      }, () => {
+        this.toolService.hideLoading();
+        console.log('complete');
+      });
   }
 
   /**
-   * retourne la liste des ministeres
-   * @returns 
+   * click sur le bouton Supprimer pour supprimmer un ministère
    */
-   getMinisteres(){
-    return [
-      { id: '0001', code: 'MJEU', libelle: 'JEUNE', description: 'Ministere des jeunes' },
-      { id: '0002', code: 'MHOM', libelle: 'HOMME', description: 'Ministere des hommes' }
-    ]
-  }
-
-  /**
-   * click sur le bouton Ajouter du modal pour enregister un ministere
-   */
-  onSubmitForm(){
-    if(this.ministerForm.valid){
-      console.log(this.ministerForm.value);
-    }
-  }
-
-  /**
-   * click sur le bouton Valider du modal pour edition
-   */
-  onUpdateForm(){
-    // console.log(this.code.errors.required);
-    if(this.editModeMinisterForm.valid){
-      console.log(this.editModeMinisterForm.value);
-    }
-  }
-
-  /**
-   * click sur le bouton Fermer du modal
-   */
-  onModalClose(){
-    this.editMode = false;
-    this.editModeMinisterForm.disable();
-    this.editModeMinisterForm.reset();
-  }
-
-  /**
-   * 
-   * @param minister 
-   * reccuperer le ministere actuel en cliquant sur une ligne du tableau 
-   */
-  getCurrentMinister(minister: Ministere){
-    this.editFormTitle = "Detail";
-    this.editModeMinisterForm.get("id").setValue(minister.id);
-    this.editModeMinisterForm.get("code").setValue(minister.code);
-    this.editModeMinisterForm.get("libelle").setValue(minister.libelle);
-    this.editModeMinisterForm.get("description").setValue(minister.description);
-    
-    if(minister != null){
-      console.log(minister);
-      this.minister = minister;
-    }
-  }
-
-  /**
-   * click sur le bouton Editer pour passer en mode edition
-   */
-  enableEditMode(){
-    this.editMode = true;
-    this.editFormTitle = "Edition";
-    // this.editModeProfileForm.get("code").enable();
-    this.editModeMinisterForm.enable();
-  }
-
-  /**
-   * Fermerture du modal automatique
-   */
-  closeModalEdit(): void {
-    this.closeBtnEdit.nativeElement.click();
-  }
-
-  /**
-   * click sur le bouton Supprimer pour supprimmer un ministere
-   */
-  deleteMinister(){
-    console.log("idMinister => "+this.idEdit.value)
-    this.toolService.showConfirmation("Suppression", "Voulez-vous supprimer ce ministere ?", "question", "Oui",
+  deleteProfile(minister: Ministere){
+    // console.log("id => "+this.idEdit.value)
+    this.toolService.showConfirmation("Suppression", "Voulez-vous supprimer ce élément ?", "question", "Oui",
       "Non", false).then((result) =>{
         if(result.isConfirmed){
-          this.toolService.removeElementFromObjectArray(this.ministeres, this.idEdit.value);
-          
+          this.toolService.showLoading();
+          this.apiService.delete(Url.PROFILE_DELETE_URL+"/"+minister.id, {}).subscribe(
+            (data) => {
+              this.toolService.showToast('Suppression réussie', 'OK', 3000);
+              this.getMinistersList();
+            }, (error) => {
+              this.toolService.hideLoading();
+              this.toolService.showToast(error.message, 'OK');
+            }, () => {
+              this.toolService.hideLoading();
+            });
         } else if (result.dismiss === Swal.DismissReason.cancel) {
           Swal.fire(
             '',
@@ -167,8 +104,7 @@ export class MinistereComponent implements OnInit {
           )
         }
       }).finally(()=>{
-        //fermerture du modal
-        this.closeModalEdit();
+        this.getMinistersList();
       })
   }
 
